1096d35254047752b69cfd20fb47e5ba8e03e7ca
Kishan Salgado <ksalgado@ksalgado-mn2.linkedin.biz>
2020-10-18 12:16:49 -0400

    Adding data to website

---
+++ wwwroot/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace gha_newwebapp.wwwroot.Models
+{
+    public class Product
+    {
+        
+        public string Id { get; set; }
+
+        public string Maker { get; set; }
+
+        [JsonPropertyName("img")]
+
+        public string Image { get; set; }
+
+        public string Url { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public int[] Ratings { get; set; }
+
+        public override ToString() => JsonSerializer.Serialize<Product>(this);
+     
+    }   
+}
+
